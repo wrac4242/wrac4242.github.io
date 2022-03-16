d9d5097483bed0666a79c655ef806adc1cbd4f86
wrac4242 <wrac4242@gmail.com>
2022-03-16 20:29:38 +0000

    better nav bar and setup

---
+++ src/About.tsx
@@ -0,0 +1,22 @@
+import './App.css';
+import React from 'react';
+
+class About extends React.Component {
+    
+    render() {
+        return (
+            <div className="App">
+				<div className="App-header">
+                <h2>About Page</h2>
+                <main>
+                    <p>This section contains information about...</p>
+                </main>
+				</div>
+            </div>
+        )
+    }
+}
+
+
+
+export default About;
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Route, Routes } from "react-router-dom";
+import About from './About';
+import Home from './Home';
+import NavBar from './NavBar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+      return (
+        <div className="Index">
+          <div>
+            <NavBar/>
+          </div>
+            <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>} />
+          </Routes>
+          </div>
+            );
+  }
 }
 
 export default App;
