ec23a7429286a7d4e02784bfd0aaa489972d4723
wrac4242 <wrac4242@gmail.com>
2022-03-16 21:44:18 +0000

    finally got background working correctly and site starting to come together

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,25 +1,18 @@
 import './App.css';
 import React from 'react';
-import logo from './logo.svg';
 
 class Home extends React.Component {
 	render() {
 		return (
 			<div className="App">
 				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
 					<p>
-						Edit <code>src/App.js</code> and save to reload.
+						Who am I?
 					</p>
-					<a
-						className="Link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
 				</header>
+				<p>
+					test
+				</p>
 			</div>
 
 		);
