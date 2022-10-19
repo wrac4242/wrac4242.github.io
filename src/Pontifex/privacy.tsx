ef297f1c0e953efc295d83fbc89f7e30710f9cd3
Wrac4242 <wrac4242@gmail.com>
2022-10-19 14:06:41 +0100

    update privacy policy

--- src/Pontifex/privacy.tsx
+++ src/Pontifex/privacy.tsx
@@ -9,20 +9,28 @@ class PontifexPrivacy extends React.Component {
 				</div>
 				<main>
 					<div className="Boxed">
-						<p>
-						What data we collect: <br></br>
-						Currently the only information we store is the logging and confession channel IDs and the server IDs even after the bot has been removed, <br></br> 
-						this information is viewable with the /serverdump command as we need this data to properly log and send confession messages<br></br>
-						No one besides the developers of the bot have access to the data and access will not be provided to people who are unauthorized to see it<br></br>
-						<br></br>
-						If you have any concerns please join the support server linked <a href="https://discord.gg/eNRsbGv8y2">here</a>, and DM a support member<br></br>
-						Support can also be offered via email at <a href="mailto:pontifex@wrac4242.com">pontifex@wrac4242.com</a>, however response times may vary<br></br>
-						Confession data (confession itself and sender) is logged into the relevant server and it is up to the server how that logged info is used, <br></br>
-						the bot does not store this confession or its information and so it cannot be used by the developer or anyone with access
-						<br></br><br></br>
-						If you wish to request or delete data stored by the bot, please join the support server linked <a href="https://discord.gg/eNRsbGv8y2">here</a>, and DM a support member<br></br>
-						or email at <a href="mailto:pontifex@wrac4242.com">pontifex@wrac4242.com</a>
-						</p>
+					<h2>What data we collect:</h2>
+					<br></br>
+					<h3>What the bot can access live:</h3>
+					<p>
+						In the bots database we store the Logging Channel ID, Confession Channel ID, and Guild ID <br></br>
+						This information is only accessible to admins of the bot and the owner of the guild (available through the /server dump command)<br></br>
+						This data may be retained after the bot has been removed from a guild.
+					</p>
+					<br></br>
+					<h3>Long term storage:</h3>
+					<p>
+						The basics of all interactions are logged in order to improve the quality of the bot and help prevent misuse. <br></br>
+						This data may include Guild and User IDs, Bot request errors and successes. This does not include the contents of the confessions.<br></br>
+						Only bot admins have access to these logging items. <br></br>
+						These are retained for the lifetime of the bot.<br></br>
+					</p>
+					<br></br>
+					<h3>Data Request</h3>
+					<p>
+						If you wish to request a copy of or delete data stored by the bot or need to contact the bot admins in any way <br></br> 
+						please join the support server <a href="https://discord.gg/eNRsbGv8y2">here</a> and ask to contact a bot support member or via email at <a href="mailto:pontifex@wrac4242.com">pontifex@wrac4242.com</a>.
+					</p>
 					</div>
 				</main>
 			</div>
